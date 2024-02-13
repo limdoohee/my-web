1bcc397f510b47521625b620694f556fe10883dd
Lim <limdoohee@gmail.com>
2024-02-13 12:41:22 +0900

    [Feat] HeroParallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { TextGenerateEffectDemo } from "./TextGenerateEffectDemo";
+import { HeroParallaxDemo } from "./Parallax";
 
 export default function Home() {
   return (
     <main>
       <TextGenerateEffectDemo />
+      <HeroParallaxDemo />
     </main>
   );
 }
