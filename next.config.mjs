92e449a28174355dce6349de8eae4f8f9b3690d8
Lim <lim@Lims-MacBook-Pro.local>
2024-02-13 12:25:27 +0900

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+    remotePatterns: [
+      {
+        hostname: "aceternity.com"
+      },
+      {
+        hostname: "images.unsplash.com"
+      },
+    ],
+  },};
 
 export default nextConfig;
