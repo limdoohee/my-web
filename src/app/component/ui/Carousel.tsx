f0985b48ccf1f1b5a996f40a2fe3c5d4750ee976
Lim <limdoohee@gmail.com>
2024-02-21 15:59:02 +0900

    [Feat] StoryBook 추가

---
+++ src/app/component/ui/Carousel.tsx
@@ -0,0 +1,77 @@
+import React, { useRef, useState } from "react";
+import "./carousel.css";
+
+////////////////////////////////////////////////////////// library import
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Thumbs,
+  Autoplay,
+  Controller,
+  EffectFade,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+// import "swiper/css/navigation";
+////////////////////////////////////////////////////////// library import
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const Carousel = () => {
+  const swiperElRef = useRef(null);
+  const [isSwiperStop, setIsSwiperStop] = useState(false);
+
+  const data = [
+    "https://asset.dropkitchen.xyz/contents/202308_prod/20230830102558820_dk.webp",
+    "https://asset.dropkitchen.xyz/contents/202308_prod/20230817130416910_dk.webp",
+    "https://asset.dropkitchen.xyz/contents/202304_dev/20230405163917111_dk.webp",
+    "https://asset.dropkitchen.xyz/contents/202308_prod/20230808181206566_dk.webp",
+    "https://asset.dropkitchen.xyz/contents/202309_prod/20230901105942218_dk.webp",
+  ];
+
+  const pagination = {
+    bulletActiveClass: `swiper-pagination-bullet-active${
+      isSwiperStop ? " stop" : ""
+    }`,
+    el: ".swiper-custom-pagination",
+    enabled: true,
+    clickable: true,
+    renderBullet: function (index: number, className: string) {
+      return `
+      <span class="${className}">
+          <img class="swiper-pagination-image" src="${data[index]}">
+          <svg width="50" height="50" class="progress"><rect width="50" height="50" /></svg>
+      </span>
+      `;
+    },
+  };
+
+  return (
+    <>
+      <Swiper
+        ref={swiperElRef}
+        effect={"fade"}
+        modules={[EffectFade, Pagination, Autoplay]}
+        pagination={pagination}
+        // autoplay={{ delay: 5000, disableOnInteraction: false }}
+        loop={true}
+        className="swiper-custom"
+      >
+        {data.map((item, key) => (
+          <SwiperSlide key={key}>
+            <img src={item} className="swiper-image" loading="lazy" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swiper-pagination-wrapper">
+        <div className="swiper-custom-pagination"></div>
+      </div>
+    </>
+  );
+};
+
+export default Carousel;
---
+++ src/stories/Carousel.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Carousel from "../app/component/ui/Carousel";
+
+const meta = {
+  title: "Example/Carousel",
+  component: Carousel,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof Carousel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Progress: Story = {};
