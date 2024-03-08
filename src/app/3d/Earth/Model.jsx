6f34b2d2b8467be3c318c7edc5025fea3bf5a547
Lim <limdoohee@gmail.com>
2024-03-08 13:13:15 +0900

    [Rename] 경로

---
+++ src/app/3d/Earth/Model.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF("/models/Earth.glb");
+  return (
+    <mesh
+      rotation={[0.6, Math.PI / 3.3, 0]}
+      scale={0.01}
+      castShadow
+      geometry={nodes.Cube001.geometry}
+      material={materials["Default OBJ"]}
+    />
+  );
+}
+
+useGLTF.preload("/models/Earth.glb");
