e48c21152ae163febe80c4fd7390b26e58ec2d99
Lim <limdoohee@gmail.com>
2024-03-10 16:26:16 +0900

    [Rename] 경로

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import React from "react";
-import Carousel from "./component/ui/Carousel";
-
 export default function Home() {
   return (
     <>
-      <Carousel />
+      <div>home</div>
     </>
   );
 }
---
+++ src/app/ui/carousel/progress/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import Carousel from "../../../component/ui/Carousel";
+
+export default function Home() {
+  return (
+    <>
+      <Carousel />
+    </>
+  );
+}
